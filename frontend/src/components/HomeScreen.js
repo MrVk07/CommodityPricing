d7d85aa3f95e069225bc174c0b8b7a7d08af4ddc
Vineet Karwa <vineetkarwa123@gmail.com>
2022-08-22 17:34:24 +0530

    text responsive

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function HomeScreen() {
     return (
-        <div>
-            homescreen
+        <div className="container homescreen">
+            <div className="row">
+                <div className="neons col-12">
+                    <h1 className='display'><em>Commodity<br />Pricing</em></h1>
+                </div>
+            </div>
         </div>
+
     )
 }
 
