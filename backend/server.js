0b6c853d5edae2e96c78d740b29d9450106c0eee
Vineet Karwa <vineetkarwa123@gmail.com>
2022-08-22 15:22:07 +0530

    css changes and deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
 import Cereals from './WebsiteScrapper/Cereals.js'
 import Fruits from './WebsiteScrapper/Fruits.js'
 import OilSeeds from './WebsiteScrapper/OilSeeds.js'
@@ -7,17 +9,27 @@ import spices from './WebsiteScrapper/Spices.js'
 
 const app = express()
 
+dotenv.config()
 const port = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    res.send("meeeeeeeeeeeessage")
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/pulses', pulses)
 app.use('/spices', spices)
 app.use('/oilseeds', OilSeeds)
 app.use('/cereals', Cereals)
 app.use('/fruits', Fruits)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
--- frontend/src/components/EachItem.js
+++ frontend/src/components/EachItem.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 function EachItem(props) {
     let { item } = props
+    let imagelink = item.image.includes(".jpg.jpg")
+        ? item.image.replace(".jpg.jpg", ".jpg")
+        : item.image
     return (
         <>
-            <div className="card border border-secondary">
-                <img className="card-img-top w-50 mt-2 mx-auto rounded" src={item.image} alt={item.name} />
+            <div className="card border border-secondary eachitem h-100">
+                <img className="card-img-top w-50 mt-2 mx-auto rounded h-100" src={imagelink} alt={item.name} />
                 <div className="card-body">
-                    <h5 className="card-title"><strong>{item.name}</strong></h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Category: <button className='btn btn-secondary'> {item.Category}</button></h6>
+                    <h5 className="card-title text-center"><strong>{item.name}</strong></h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Category: <button className='btn btn-danger'> {item.Category}</button></h6>
                     <p className="card-text">Costliest Market :<strong>{item.Costliest_Market}</strong></p>
                     <p className="card-text">Costliest Market Price :<strong>{item.Costliest_Market_Price}</strong></p>
                     <p className="card-text">Cheapest Market :<strong>{item.Cheapest_Market}</strong></p>
